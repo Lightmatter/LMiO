119c3112971040bb9f27932ad962a5874678fe2d
Gregory Azevedo <g@lightmatter.com>
2017-08-21 15:29:03 +0200

    initial commit

---
+++ LMiOSTemplate/LMiOSTemplate.h
@@ -0,0 +1,19 @@
+//
+//  LMiOSTemplate.h
+//  LMiOSTemplate
+//
+//  Created by Gregory Azevedo on 8/21/17.
+//  Copyright © 2017 Lightmatter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LMiOSTemplate.
+FOUNDATION_EXPORT double LMiOSTemplateVersionNumber;
+
+//! Project version string for LMiOSTemplate.
+FOUNDATION_EXPORT const unsigned char LMiOSTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LMiOSTemplate/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LMiOSTemplate' do
+  use_frameworks!
+
+  # Pods for LMiOSTemplate
+  pod 'Moya'
+
+  target 'LMiOSTemplateTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+end
